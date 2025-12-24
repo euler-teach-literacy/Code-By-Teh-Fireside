716ac58bf40d6195b7cb599b9c07aff23fe95abf
euler-teach-literacy <brian12not13@gmail.com>
2025-12-24 17:37:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ const pargraph1 = document.getElementById("paragraph")
 pargraph1.textContent = "paragraph"
 
 //ts so tuff
+
+
+
+
+// clicking sound: keyboard click
+var clicking_time = 0
+
+const kbc_b = document.querySelector('.click_button');
+const kbc_s = new Audio('resources/keyboard_click.mp3');
+
+kbc_b.addEventListener('click', () => {
+  kbc_s.play();
+});
+
+if (kbc_s.play){
+  clicking_time += 1;
+};
+// // // / /// // // // / // / / // /
